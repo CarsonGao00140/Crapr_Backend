a8bf85e33121a57d3ac969b7e6b225ca18482266
CarsonGao00140 <156142273+CarsonGao00140@users.noreply.github.com>
2024-08-24 00:03:27 -0400

    Test

--- src/middlewares/isAuthenticated.js
+++ src/middlewares/isAuthenticated.js
@@ -3,7 +3,9 @@ import passport from 'passport';
 import { UnauthenticatedError } from '../utilities/error.js';
 
 export default (req, res, next) => {
+    console.log(eq.headers.authorization);
     req.headers.authorization ||= `bearer ${cookie.parse(req.headers.cookie || '').token}`;
+    console.log(eq.headers.authorization);
     if (!req.headers.authorization)
         throw new UnauthenticatedError("JWT token not provided.");
 
